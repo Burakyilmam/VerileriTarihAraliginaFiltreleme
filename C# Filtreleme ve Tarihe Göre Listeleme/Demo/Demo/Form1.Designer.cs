223a80185d72c6d29c5879bd6c6799437487e9e6
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2021-08-31 00:44:50 +0300

    Add files via upload

---
+++ C# Filtreleme ve Tarihe Göre Listeleme/Demo/Demo/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnFiltre = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.updateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(597, 182);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(353, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(351, 23);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 6, 25, 0, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(353, 41);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(351, 23);
+            this.dateTimePicker2.TabIndex = 5;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 6, 25, 0, 0, 0, 0);
+            // 
+            // btnFiltre
+            // 
+            this.btnFiltre.BackColor = System.Drawing.Color.Silver;
+            this.btnFiltre.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnFiltre.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFiltre.Location = new System.Drawing.Point(765, 9);
+            this.btnFiltre.Name = "btnFiltre";
+            this.btnFiltre.Size = new System.Drawing.Size(240, 55);
+            this.btnFiltre.TabIndex = 2;
+            this.btnFiltre.Text = "FİLTRELE";
+            this.btnFiltre.UseVisualStyleBackColor = false;
+            this.btnFiltre.Click += new System.EventHandler(this.btnFiltre_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 40;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1061, 555);
+            this.dataGridView1.StandardTab = true;
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.Silver;
+            this.updateBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.updateBtn.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.updateBtn.Location = new System.Drawing.Point(65, 12);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(240, 55);
+            this.updateBtn.TabIndex = 1;
+            this.updateBtn.Text = "GÜNCELLE";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1061, 628);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnFiltre);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Gönderilemeyen Mailleri Yeniden Gönderilebilir Duruma Getirme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnFiltre;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button updateBtn;
+    }
+}
+
---
+++ C# Filtreleme ve Tarihe Göre Listeleme/Demo/Demo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        string constring = ("Data source =  192.168.7.19,1433;initial catalog = CPM; integrated security = True; User ID=sa;Password=sql;Trusted_Connection=False;");
+        String firma = ConfigurationManager.AppSettings["FİRMA"];
+        String donem = ConfigurationManager.AppSettings["DÖNEM"];
+        SqlConnection cn = null;
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today;
+            dateTimePicker2.Value = DateTime.Today;
+            constring = ConfigurationManager.AppSettings["connectionstrings"];
+            firma = ConfigurationManager.AppSettings["FİRMA"];
+            donem = ConfigurationManager.AppSettings["DÖNEM"];
+            cn = new SqlConnection(constring);
+        }
+   
+        private void listBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFiltre_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter("SELECT BAS.BELGENO,BAS.TARIH,FT.FIRMAADI,ST.STOKKODU AS \"REFERANS NUMARASI\",FT.ISTIHBARAT MAIL,ST.MALINCINSI,KARSISTOKKODU AS \"Zn+Ni Min-Max\",KARSIBARKOD AS \"KTL Min-Max\",BARKOD AS \"Zn Min-Max\",FDT.PIRIM AS \"%Ni\",MAILRESPONSE FROM " + firma + donem + "TBLSATIRSHAREKET FDT,"+firma+ "TBLCARI FT,"+firma + "TBLSTOKLAR ST," + firma + donem + "TBLSATIRSBASLIK BAS WHERE FDT.FIRMANO=FT.IND and FDT.STOKNO =ST.IND and FDT.EVRAKNO=BAS.IND and (ISNULL(FDT.PIRIM,0)!=0 OR (ISNULL(BARKOD,'')!='') OR ISNULL(KARSISTOKKODU,'')!='' OR ISNULL(KARSIBARKOD,'')!='') AND MAILSTATUS=0 AND MAILRESPONSE IS NULL and BAS.TARIH BETWEEN @tarih1 and @tarih2 ORDER BY FDT.FIRMANO ", cn);
+            adp.SelectCommand.Parameters.AddWithValue("@tarih1", dateTimePicker1.Value);
+            adp.SelectCommand.Parameters.AddWithValue("@tarih2", dateTimePicker2.Value);
+            cn.Open();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cn.Close();       
+        }
+
+        private void updateBtn_Click_1(object sender, EventArgs e)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("Update " + firma + donem + "TBLSATIRSHAREKET SET MAILSTATUS=NULL where MAILSTATUS=0", cn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("MAILSTATUS DEĞERİ 0 OLAN DEĞERLER NULL OLARAK GÜNCELLENDİ.");
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            cn.Close();
+        }
+    }
+}
